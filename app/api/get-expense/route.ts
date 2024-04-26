1069ab6a7b168c6dcb7ab1d526490de0de7484dd
brian890809 <brian08090809@gmail.com>
2024-04-26 11:26:27 -0700

    solved router return empty obj issue

--- app/api/get-expense/route.ts
+++ app/api/get-expense/route.ts
@@ -6,12 +6,11 @@ export async function GET(req: Request) {
   let recentExpenses
   try {
     const client: MongoClient = await clientPromise
-    const result = await FindExpenses(client, req)
-    recentExpenses = JSON.parse(JSON.stringify(result))
+    recentExpenses = await FindExpenses(client, req)
   } catch (e) {
     console.error(e)
     location.reload()
     recentExpenses = []
   }
-  return new NextResponse(recentExpenses)
+  return new NextResponse(JSON.stringify(recentExpenses))
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,13 @@
 import { RecentExpenses } from '@/component'
-import { clientPromise, FindExpenses } from '@/api'
-import { MongoClient } from 'mongodb'
 
 const Header = ({ title }) => {
   return <h1>{title ? title : 'Default title'}</h1>
 }
 
 const HomePage = async () => {
-  const testingres = await fetch('http://localhost:3000/api/get-expense')
-  const testing = JSON.stringify(testingres)
-  console.log('response: ', testing)
-  let recentExpenses
+  const result = await fetch('http://localhost:3000/api/get-expense')
+  const recentExpenses = await result.json()
 
-  try {
-    const client: MongoClient = await clientPromise
-    const result = await FindExpenses(client, {})
-    recentExpenses = JSON.parse(JSON.stringify(result))
-  } catch (e) {
-    console.error(e)
-    location.reload()
-    recentExpenses = []
-  }
   return (
     <div>
       <Header title="Develop. Preview. Ship. 🚀" />
