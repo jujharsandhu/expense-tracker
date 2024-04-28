2dec2c18e7fcafb1a4b9ef68a1e56ac86ed5e42b
brian890809 <brian08090809@gmail.com>
2024-04-27 17:54:19 -0700

    router POC scaffold complete

--- app/api/add-expense/route.ts
+++ app/api/add-expense/route.ts
@@ -1,21 +1,25 @@
 import { clientPromise, CreateExpense } from '@/api'
 import { MongoClient } from 'mongodb'
+import { NextResponse } from 'next/server'
 
 export async function GET(req) {
   return new Response('This is a new API route')
 }
 
-export async function PUT(req) {
-  console.log(req)
+export async function POST(req: Request) {
+    
+  const requestBody = await req.json()
+  console.log('requestBody: ', requestBody)
   try {
     const client: MongoClient = await clientPromise
     console.log('connected')
-    const result = await CreateExpense(client, req)
-    console.log(result)
+    // const result = await CreateExpense(client, req)
+    // console.log(result)
     // recentExpenses = JSON.parse(JSON.stringify(result))
   } catch (e) {
     console.error(e)
     location.reload()
     // recentExpenses = []
   }
+  return new NextResponse("hello i am put")
 }
